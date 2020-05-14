54809c297e63b3d78e185af6d933d73952c62775
KirillGorbach <kirill.gorbach@gmail.com>
2020-05-14 12:14:42 +0300

    Перед мержем

--- app/src/main/java/com/poker/holdem/server/deserialization/playerleft/DesPlayerLeftResp.java
+++ app/src/main/java/com/poker/holdem/server/deserialization/playerleft/DesPlayerLeftResp.java
@@ -7,6 +7,7 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 
 import java.lang.reflect.Type;
+import java.util.logging.Logger;
 
 public class DesPlayerLeftResp implements JsonDeserializer<PlayerLeftResp> {
     @Override
@@ -14,7 +15,17 @@ public class DesPlayerLeftResp implements JsonDeserializer<PlayerLeftResp> {
         JsonObject jsonObject = json.getAsJsonObject();
         PlayerLeftResp playerLeftResp = new PlayerLeftResp();
         playerLeftResp.setName(jsonObject.get("name").getAsString());
-        playerLeftResp.setNewlead(jsonObject.get("newlead").getAsString());
+        try {
+            playerLeftResp.setNewlead(jsonObject.get("newlead").getAsString());
+        }catch (Exception e){
+            //если мы были последним игроком в комнате, то, конечно же,
+            //больше ведущего игрока там не будет от слова соысем
+            //в этом случае мы не будем использовать это поле,
+            //и просто инициализируем его пустой строкой
+            playerLeftResp.setNewlead("");
+            Logger.getAnonymousLogger().info("<-If we leave lobby it's ok. If not, check why \"playerleft\" \"newlead\" is null.");
+        }
+
         playerLeftResp.setNewround(jsonObject.get("newround").getAsBoolean());
         return playerLeftResp;
     }
